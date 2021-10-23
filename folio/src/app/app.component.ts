7cb5df7a983768a142dca25114229bfe9ee99418
Radosław Jakubowski <radoslaw.jakubowski@gotoma.pl>
2021-10-23 19:21:42 +0200

    first page layout ideas

--- folio/src/app/app.component.ts
+++ folio/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private colours: string[] = ['red', 'blue', 'orange'];
+  public currentColour: string = '';
   title = 'folio';
+
+  ngOnInit(): void {
+  }
+
+  private randomIntFromInterval(min: number, max: number) : number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 }
