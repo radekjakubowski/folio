e26667c128e17026320966e25d48c7582c92d7ce
Radosław Jakubowski <radoslaw.jakubowski@gotoma.pl>
2021-10-24 11:13:34 +0200

    made components for about, techs and contact. Started styling for mobile.

--- folio/src/app/app-routing.module.ts
+++ folio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MenuComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'technologies', component: TechnologiesComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- folio/src/app/app.module.ts
+++ folio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TechnologiesComponent,
+    ContactComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
