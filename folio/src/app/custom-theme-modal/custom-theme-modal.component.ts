096ca7676228034c294be86bdbd2920e4348b6b9
Radosław Jakubowski <radoslaw.jakubowski@gotoma.pl>
2022-03-04 17:11:12 +0100

    hex darkening

--- folio/src/app/custom-theme-modal/custom-theme-modal.component.ts
+++ folio/src/app/custom-theme-modal/custom-theme-modal.component.ts
@@ -37,7 +37,7 @@ export class CustomThemeModalComponent {
   }
 
   applyTheme() {
-    const bgColorRgb = this.hexToRgbWithDarken(this.main);
+    const bgColorRgb = this.lightenDarkenColor(this.main, -60);
     this.utilitiesService.setTheme('');
     document.documentElement.style.setProperty('--default-main', this.main);
     document.documentElement.style.setProperty('--default-secondary-main', this.secondaryMain);
@@ -53,14 +53,30 @@ export class CustomThemeModalComponent {
     this.utilitiesService.isSettingsModalOpen = true;
   }
 
-  private hexToRgbWithDarken(hex: string) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    const rgb = result ? {
-      r: parseInt(result[1], 16) - 20,
-      g: parseInt(result[2], 16) - 20,
-      b: parseInt(result[3], 16 - 20)
-    } : null;
+  private lightenDarkenColor(col: string, amt: number) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
 
-    return `(${rgb?.r}, ${rgb?.g}, ${rgb?.b})`
-  }
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
 }
